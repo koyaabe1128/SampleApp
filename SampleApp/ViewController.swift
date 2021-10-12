6965b5527930f1ddf3436ab15fec2befa5982780
koyaabe <koyatendo@gmail.com>
2021-10-12 23:28:37 +0900

    init

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let todoArray = ["スーパー行く","郵便局行く","靴買う","勉強する","9時には寝る"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        cell.textLabel!.text = todoArray[indexPath.row]
+        return cell
+    }
 
 }
 
